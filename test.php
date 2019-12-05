cd7a06ccade0b905d1a602cdcce97b997001b129
samuel <samuelmanikiza@gmail.com>
2019-12-05 15:11:09 +0200

    Changes

--- test.php
+++ test.php
@@ -1,7 +1,8 @@
 <?php 
 require 'classes_loader.php';
 
-$training_id=5;
-$pc_id=$upload->getFreeTable($training_id);
-var_dump($pc_id);
+$training_id=6;
+$pc_id=238;
+$change_status=$upload->changeComputerStatus($pc_id,$training_id);
+var_dump($change_status);
 ?>
